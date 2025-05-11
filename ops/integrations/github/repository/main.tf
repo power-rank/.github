9576dc957796d1f670484f7e82fad92f6f02b066
Raul Araya <nubeiro@gmail.com>
2025-05-11 12:35:10 +0200

    Add ops folder
    Signed-off-by: Raul Araya <nubeiro@gmail.com>

---
+++ ops/integrations/github/repository/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.6"
+    }
+  }
+}
+
+provider "github" {
+  owner = local.github_owner
+}
+
+resource "github_repository" "readme" {
+  name        = local.repository
+  description = "README.md repository"
+  visibility  = "public"
+  has_issues  = false
+  auto_init   = false
+  gitignore_template = "Terraform"
+  delete_branch_on_merge = true
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+locals {
+  github_owner = "power-rank"
+  repository   = ".github"
+}
