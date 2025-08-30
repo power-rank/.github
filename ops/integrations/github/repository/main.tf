f6e0477226284899fb56b6bfc348210e1204ce84
Raul Araya <nubeiro@gmail.com>
2025-08-30 19:41:08 +0200

    Hola Judit ;-)

--- ops/integrations/github/repository/main.tf
+++ ops/integrations/github/repository/main.tf
@@ -12,16 +12,21 @@ provider "github" {
 }
 
 resource "github_repository" "readme" {
-  name        = local.repository
-  description = "README.md repository"
-  visibility  = "public"
-  has_issues  = false
-  auto_init   = false
-  gitignore_template = "Terraform"
+  name                   = local.repository
+  description            = "README.md repository"
+  visibility             = "public"
+  has_issues             = false
+  auto_init              = false
+  gitignore_template     = "Terraform"
   delete_branch_on_merge = true
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "github_membership" "invite_user" {
+  username = "Judit716"
+  role     = "member"
 }
 
 locals {
